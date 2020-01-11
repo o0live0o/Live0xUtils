232467b2033a00a309f2d42e86862bed1206ed94
o0live0o <49543424+o0live0o@users.noreply.github.com>
2020-01-11 17:23:34 +0800

    Add Deep Clone

---
+++ Live0xUtils/CloneHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Live0xUtils
+{
+     public abstract class CloneHelper
+    {
+        //深拷贝
+        public static object Clone<T>(T t)
+        {
+            try
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, t);
+                memoryStream.Position = 0;
+                return binaryFormatter.Deserialize(memoryStream);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
